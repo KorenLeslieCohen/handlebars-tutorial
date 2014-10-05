1c9b05d54230cdd740873c93f01dff629fe4e072
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-10-05 17:16:45 -0400

    readme and jquery template

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a Handlebars.js tutorial I prepared. The full tutorial may be viewed on my blog:
--- js/animalList.js
+++ js/animalList.js
@@ -1,9 +1,24 @@
 // ANIMAL TEMPLATE
 
+// sets variable source to the animalTemplate id in index.html
 var source = document.getElementById("animalTemplate").innerHTML;
+
+// Handlebars compiles the above source into a template
 var template = Handlebars.compile(source);
-var context = {type: "Dog", sound: "woof"};
-var output = template(context);
+
+// data
+var data = {type: "Dog", sound: "woof"};
+
+// data is passed to above template
+var output = template(data);
+
+// HTML element with id "animalList" is set to the output above
 document.getElementById("animalList").innerHTML = output;
 
+// JQUERY ANIMAL TEMPLATE
+
+// var source = $("#animalTemplate").html();
+// var template = Handlebars.compile(source);
+// var data = {type: "Dog", sound: "woof"};
+// $("#animalList").html(template(data));
 
